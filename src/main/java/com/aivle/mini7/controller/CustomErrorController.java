d80fad2b4b92bb378c9cbb3b9513ecd20c221ec0
yeonju <919jung@gmail.com>
2024-12-26 12:05:09 +0900

    error page2

---
+++ src/main/java/com/aivle/mini7/controller/CustomErrorController.java
@@ -0,0 +1,16 @@
+package com.aivle.mini7.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // error.html로 모든 오류 처리
+        return "error";
+    }
+}
